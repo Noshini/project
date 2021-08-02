330d191f4ed30ddf898c0aa171e459311d6ba5bf
Noshini <88227859+Noshini@users.noreply.github.com>
2021-08-02 10:57:56 +0530

    Add files via upload

---
+++ websitealarm.py
@@ -0,0 +1,12 @@
+import time
+import webbrowser
+Set_Alarm= input("Set the website alarm as H:M:S(all in two digits):")
+url = input("Enter the website you want to open:")
+Actual_Time = time.strftime("%H:%M:%S")
+while(Actual_Time != Set_Alarm):
+    print("The time is" + Actual_Time)
+    Actual_Time = time.strftime("%H:%M:%S")
+    time.sleep(1)
+if(Actual_Time == Set_Alarm):
+    print("You should see your webpage now:-)")
+    webbrowser.open(url)
